bae708382a20e5513c820ae9211835694e817624
Jonathan Woodard <jonathan@jlwoodard.com>
2015-02-02 21:51:07 -0600

    Initial commit

---
+++ dailydevo.py
@@ -0,0 +1,18 @@
+__author__ = 'jonathan'
+
+import requests
+from bs4 import BeautifulSoup
+import html2text
+
+soup = BeautifulSoup(requests.get("http://www.blackaby.org/devarchive.asp").text)
+
+page = soup.find('div', id='content-right')
+
+title = page.find('span', class_="titleHome")
+print title.text + '\n'
+
+date = page.find('div', class_='sidetitle')
+print date.text + '\n'
+
+devotional = page.find("div", class_='content-body')
+print html2text.html2text(devotional.prettify())
---
+++ votd.py
@@ -0,0 +1,15 @@
+__author__ = 'jonathan'
+
+import requests
+from lxml import etree
+import HTMLParser
+import re
+
+xml = etree.fromstring(requests.get("https://www.biblegateway.com/usage/votd/rss/votd.rdf?49").text)
+verse = xml.xpath("/rss/channel/item/title")[0].text
+scripture = xml.xpath("/rss/channel/item/content:encoded", namespaces=xml.nsmap)[0]
+h = HTMLParser.HTMLParser()
+scripture = re.sub(r"<br/>.*", '', h.unescape(scripture.text).strip())
+
+print scripture
+print "-- "+verse+" NASB"
